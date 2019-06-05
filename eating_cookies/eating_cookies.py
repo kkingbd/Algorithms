7364329873dccc956f05b0425e88e066965209a9
Kawsar Hussen <kawsarhussen@gmail.com>
2019-06-05 17:04:20 -0400

    Formatting fixed

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,15 +6,15 @@ import sys
 # a solution that is more efficient than the naive
 # recursive solution
 def eating_cookies(n, cache=None):
-if n == 0:
-return 1
-elif n < 0:
-return 0
-else:
-return eating_cookies(n-1) + eating_cookies(n-2)+ eating_cookies(n-3)
+    if n == 0:
+      return 1
+    elif n < 0:
+      return 0
+    else:
+      return eating_cookies(n-1) + eating_cookies(n-2)+ eating_cookies(n-3)
 if __name__ == "__main__":
-if len(sys.argv) > 1:
-num_cookies = int(sys.argv[1])
-print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-else:
-print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
